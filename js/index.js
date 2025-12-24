d975bc2ec50745ff676e53c3f37f079d06920cfa
David <syllad119@gmail.com>
2025-12-24 08:50:19 +0100

    projet encours

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
-console.log ("hello world")
+// To fetch pour recuperer les donnes json
+
+const url = "http://127.0.0.1:5501/js/data/menu.json";
+
+const menus = document.querySelector("#menus");
+
+// methode fetch
+
+fetch(url)
+  .then((response) => {
+    console.log(response);
+    return response.json(); //recup de fichier json
+  })
+  .then((data) => {
+    console.log(data);
+    console.log(data[0].name); //affichage du fichier json7
+    // parcourir le tableau et crée des elements pour chaque mois
+    for (let index = 0; index < data.length; index++) {
+        console.log(index);
+
+      const article = document.createElement("article");
+      article.classList = "menu";
+      article.style.backgroundColor = "#BF7E04";
+      article.innerHTML = `
+            <img src="${data[index].image}" alt="${data[index].name}" >
+            <h2>${data[index].name}</h2>
+            <p>${data[index].description}</p>
+            <p>Prix: ${data[index].price} € </p>
+            <p>Categorie: ${data[index].categorie} </p>
+            `;
+      menus.appendChild(article);
+    //   console.log(menus);
+    // //   console.log(article);
+        
+    }
+    
+  })
+
+  .catch((error) => {
+    console.log(
+      "Il y a eu un probleme avec l'operation fetch: " + error.message
+    );
+  });
